af8998e9f6eecb4379e03facec3bd7b018b3dda0
Michael Lopez <mickael.lopez@gmail.com>
2024-10-07 22:35:13 +0200

    Fixing testing

--- base.py
+++ base.py
@@ -1,3 +1,5 @@
+"""Base Class to manage Arlo's Base Stations."""
+
 import logging
 import json
 from device import Device
@@ -15,31 +17,25 @@ class Base(Device):
 
     def __init__(self, arlo_base, status_interval):
         super().__init__(arlo_base, status_interval)
-        logging.info(f"Base added: {self.name}")
+        logging.info("Base added: %s", self.name)
 
     # Distributes events to correct handler
     async def on_event(self, attr, value):
         match attr:
-            case 'activeMode':
+            case "activeMode":
                 self._state_event.set()
-                logging.info(f"{self.name} mode: {value}")
+                logging.info("%s mode: %s", self.name, value)
             case _:
                 pass
 
     def get_status(self):
-        return {
-            "mode": self._arlo.mode,
-            "siren": self._arlo.siren_state
-            }
+        return {"mode": self._arlo.mode, "siren": self._arlo.siren_state}
 
     async def mqtt_control(self, payload):
         """
         Handles incoming MQTT commands
         """
-        handlers = {
-            'mode': self.set_mode,
-            'siren': self.set_siren
-        }
+        handlers = {"mode": self.set_mode, "siren": self.set_siren}
 
         try:
             payload = json.loads(payload)
@@ -47,10 +43,10 @@ class Base(Device):
                 if k in handlers:
                     self.event_loop.run_in_executor(None, handlers[k], v)
         except Exception:
-            logging.warning(f"{self.name}: Invalid data for MQTT control")
+            logging.warning("%s: Invalid data for MQTT control", self.name)
 
     def set_mode(self, mode):
-        """"
+        """ "
         Sets mode of Base Station
         """
         try:
@@ -59,21 +55,21 @@ class Base(Device):
                 raise ValueError
             self._arlo.mode = mode
         except (AttributeError, ValueError):
-            logging.warning(f"{self.name}: Invalid mode, ignored")
+            logging.warning("%s: Invalid mode, ignored", self.name)
 
     def set_siren(self, state):
         """
         Sets siren (on/off/on with specified duration and volume)
         """
         match state:
-            case 'on':
+            case "on":
                 self._arlo.siren_on()
-            case 'off':
+            case "off":
                 self._arlo.siren_off()
             case dict():
                 try:
                     self._arlo.siren_on(**state)
                 except AttributeError:
-                    logging.warning(f"{self.name}: Invalid siren arguments")
+                    logging.warning("%s: Invalid siren arguments", self.name)
             case _:
                 pass
